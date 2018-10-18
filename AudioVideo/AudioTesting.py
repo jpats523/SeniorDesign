4fe722a2b7b5da93a676e3f620cfca89a83cdf29
jpats523 <44264857+jpats523@users.noreply.github.com>
2018-10-18 11:51:05 -0400

    Create AudioTesting.py

---
+++ AudioVideo/AudioTesting.py
@@ -0,0 +1,42 @@
+import pyaudio
+import wave
+import cv2
+
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2 #stereo
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print ("recording...")
+frames = []
+
+#how we do we manually stop the recording? 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    #print(data)
+    #print('\n')
+    frames.append(data)
+    
+print ("finished recording")
+ 
+ 
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
