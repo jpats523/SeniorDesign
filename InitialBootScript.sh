6afdc07940a6b44f56612ec6d03c34335b7dcd6a
jpats523 <44264857+jpats523@users.noreply.github.com>
2018-10-18 11:28:24 -0400

    Create InitialBootScript.sh

---
+++ InitialBootScript.sh
@@ -0,0 +1,9 @@
+#this script will run everything that is needed to set up:
+#opencv 3-4-3
+
+#!/bin/bash
+echo "Running the IntialBootScript written by Jeremy"
+
+#updates the distro and the packages
+
+#runs the script to unstall opencv 3-4-3
