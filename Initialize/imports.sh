516f7b66bd39c256acae1f4e22f58fc5f5e2b904
jpats523 <44264857+jpats523@users.noreply.github.com>
2018-10-18 12:07:48 -0400

    Update imports.sh

--- Initialize/imports.sh
+++ Initialize/imports.sh
@@ -1,4 +1,14 @@
+#this file will install everything that is imported in every python script (need to confirm this works at all)
+#!/bin/bash
+
 pip3 install pyaudio #this one seems to be giving me the trouble...
 pip3 install wave
+pip3 install numpy
+pip3 install time
+pip3 install datetime
+pip3 install datetime
+pip3 install threading
+pip3 install subprocess
 
-#not sure how to make this work
+
+
